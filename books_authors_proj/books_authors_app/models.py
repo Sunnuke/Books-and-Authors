f99c2a7e8994d6f3a95dadcfeb77dccbb95cc9b2
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-05 12:40:15 -0800

    Created a class for Book and Author, created migation: created tables for the database

--- books_authors_proj/books_authors_app/models.py
+++ books_authors_proj/books_authors_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=30)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Authors(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
