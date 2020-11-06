9ba37b1b5a32a8913b96c7aa7299ce15a8556f8c
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-05 16:51:43 -0800

    Practice making migrations, and creating and manipulating objects in the database

--- books_authors_proj/books_authors_app/models.py
+++ books_authors_proj/books_authors_app/models.py
@@ -3,13 +3,15 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=30)
-    desc = models.TextField()
+    desc = models.TextField(null=True)
+    notes = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # authors (MTM/related_name)
 
-
-class Authors(models.Model):
+class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    books = models.ManyToManyField (Book, related_name="authors")
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
