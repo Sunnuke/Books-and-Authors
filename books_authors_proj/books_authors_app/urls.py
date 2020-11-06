44901624c635cbe743a7c9fc09fd50253ec16bd5
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-11-06 00:22:43 -0800

    Added two more templates that mirror the first two but this time they display database infomation for the authors and allow user input to create new authers also create new routes to process that data and store it in the database

--- books_authors_proj/books_authors_app/urls.py
+++ books_authors_proj/books_authors_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('add/book', views.addBook),
     path('books/add/authorToBook/<num>', views.addauthorToBook),
     path('books/<num>', views.viewBook),
+    path('authors', views.authors),
+    path('add/author', views.addAuthor),
+    path('authors/add/bookToAuthor/<num>', views.addbookToAuthor),
+    path('authors/<num>', views.viewAuthor),
 ]
--- books_authors_proj/books_authors_app/views.py
+++ books_authors_proj/books_authors_app/views.py
@@ -35,4 +35,34 @@ def viewBook(request, num):
     return render(request, 'view.html', context)
 
 
+# Author side
+# Loads up root and renders database
+def authors(request):
+    context = {
+        'all_books': Book.objects.all(),
+        'all_Authors': Author.objects.all()
+    }
+    return render(request, 'author.html', context)
 
+# Takes user input for a new author and adds it to the database
+def addAuthor(request):
+    Author.objects.create(first_name=request.POST['first_name'],last_name=request.POST['last_name'],notes=request.POST['notes'])
+    return redirect('/authors')
+
+# Adds a book to the viewed author
+def addbookToAuthor(request, num):
+    key = int(num)
+    boo = int(request.POST['alist'])
+    Author.objects.get(id=key).books.add(Book.objects.get(id=boo))
+    return redirect('/authors')
+
+# Takes the user to a new template and allow them to view the details of the selected author
+def viewAuthor(request, num):
+    key = int(num)
+    print(key)
+    context = {
+        'author': Author.objects.get(id=key).books.all(),
+        'viewauthor': Author.objects.get(id=key),
+        'Books': Book.objects.all(),
+    }
+    return render(request, 'authorView.html', context)
